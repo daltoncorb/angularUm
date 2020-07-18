5a89e4a4fffc151703104414a96486b2600c1897
daltoncorb <daltoncorb@yahoo.com.br>
2020-07-18 19:41:51 -0300

    terminado o app

--- src/app/Component/painel/painel.component.ts
+++ src/app/Component/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FRASES } from "./frases-mock";
 import { Frase } from "./../../Shared/frase.model";
 
@@ -12,10 +12,16 @@ export class PainelComponent implements OnInit {
   public instrucao: string = 'Traduza a frase abaixo:'
   public Resposta: string = ''
 
+  public tentativas: number = 3
   public rodada: number = 0
+  public progressoBar: number = 0
+
   public rodadaFrase: Frase
 
-  public progressoBar: number = 0
+  //pode-se especificar o tipo do eventMitter
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter()
+  // ou pode-se fazer assim
+  //@Output() public encerrarJogo = new EventEmitter()
 
   constructor() {
     //this.rodadaFrase = this.frases[this.rodada]
@@ -37,23 +43,33 @@ export class PainelComponent implements OnInit {
       alert('Você acertou a Resposta!')
       this.rodada++
       this.progressoBar += (100 / this.frases.length)
-      
+
       if (this.rodada > this.frases.length - 1) {
+        //sucesso terminou o jogo
         this.rodada = 0
         this.rodadaFrase = this.frases[0]
+        this.encerrarJogo.emit('vitoria')
       }
       else
         this.atualizarRodada()
     }
-    else
-      alert('Você errou a resposta.\nTente novamente! ')
+    else {
+      this.tentativas--
+
+      if (this.tentativas < 0){
+        alert('Suas chances acabaram.\nVocê errou muito!')
+        this.encerrarJogo.emit('derrota')
+      }
+      else
+        alert('Você errou a resposta.\nTente novamente! ')
+    }
   }
 
-  public atualizarRodada(): void{
+  public atualizarRodada(): void {
     this.rodadaFrase = this.frases[this.rodada]
     this.Resposta = ''
   }
-  
+
 
 
 }
--- src/app/Component/tentativas/tentativas.component.ts
+++ src/app/Component/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  Input, OnChanges, OnInit } from '@angular/core';
 import { Coracao } from "./../../Shared/coracao.model";
 
 @Component({
@@ -6,20 +6,34 @@ import { Coracao } from "./../../Shared/coracao.model";
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+
+export class TentativasComponent implements OnInit, OnChanges {
   //por property binding - ele muda uma tag do html
   //devem ser colocados os colchetes na tag que recebe o valor 
   // no componente html colocar <img [src] =.....
-  public cc: string = '/assets/images/cc.png'
-  public cv: string = '/assets/images/cv.png'
+  //public cc: string = '/assets/images/cc.png'
+  //public cv: string = '/assets/images/cv.png'
+
+  @Input() public tentativas: number
 
   public coracoes: Coracao[] = [ new Coracao(true), new Coracao(true), new Coracao(true)] 
 
   constructor() { 
-    //console.log(this.coracoes)
+    
+  }
+
+  ngOnChanges(): void{
+
+    if(this.tentativas !== this.coracoes.length && this.tentativas >= 0)
+    {
+      let indice = this.coracoes.length - this.tentativas
+      this.coracoes[indice - 1].isCheio = false
+    }
+   
   }
 
   ngOnInit(): void {
+    //console.log('tenttativas recebidas de foora ' + this.tentativas)
   }
 
 }
